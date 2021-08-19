bfe792ad04cf6a368ba8d090bab80a48effa2fc9
Pavel Dubov <PADking888@yandex.ru>
2021-08-19 17:12:20 +0300

    feat: Gets shortened link (bitlink)

--- click_informer.py
+++ click_informer.py
@@ -6,18 +6,25 @@ import requests
 
 load_dotenv()
 
-TOKEN = os.environ['TOKEN']
 
 def main():
+    
+    TOKEN = os.environ['TOKEN']
+    long_url = 'https://hctraktor.org/team/players/'
+
     headers = {
         'Authorization': f'Bearer {TOKEN}',
+        'Content-Type': 'application/json',
     }
-
-    url = 'https://api-ssl.bitly.com/v4/user'
-    response = requests.get(url, headers=headers)
+    url = 'https://api-ssl.bitly.com/v4/shorten'
+    payload = {
+        'long_url': f'{long_url}'
+    }
+    
+    response = requests.post(url, json=payload, headers=headers)
     response.raise_for_status()
-    print(response.json())
 
+    print(response.json()['link'])
 
 
 if __name__ == '__main__':
