fdafbcb2ebd6628caa2eb002a1d5190ccda29a06
Pavel Dubov <PADking888@yandex.ru>
2021-08-20 10:09:05 +0300

    feat: Add a click-through counter by bitlink

--- click_informer.py
+++ click_informer.py
@@ -8,6 +8,32 @@ import requests
 load_dotenv()
 
 
+def count_clicks(bitlink, token, **kwargs):
+
+    unit = kwargs.pop('unit', 'day')
+    units = kwargs.pop('units', -1)
+
+    if kwargs:  # фактические параметры всё-таки остались
+        raise TypeError(f'Неподдерживаемые параметры: {list(kwargs)}')
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+    params = (
+        ('unit', unit),
+        ('units', units),
+
+    )
+
+    short_link = get_striped(bitlink)
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{short_link}/clicks/summary'
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    clicks_count = response.json()['total_clicks']
+
+    return clicks_count
+
+
 def shorten_link(token, long_url):
     
     headers = {
@@ -37,9 +63,10 @@ def get_striped(url):
     return url_without_scheme_path
 
 
-def main():
+def get_shortened_bitlink():
     token = os.environ['TOKEN']
-    long_url = input('Введите ссылку ')
+    prompt = 'Введите ссылку '
+    long_url = input(prompt)
     
     try:
         short_link = shorten_link(token, long_url)
@@ -49,5 +76,19 @@ def main():
         print('Битлинк', short_link)
 
 
+def get_clicks_count():
+    token = os.environ['TOKEN']
+    prompt = 'Введите ссылку '
+    bitlink = input(prompt)
+    
+    try:
+        clicks_count = count_clicks(bitlink, token)
+    except requests.exceptions.HTTPError as error:
+        print(error)
+    else:
+        clicks_count_msg = f'По вашей ссылке прошли: {clicks_count} раз(а)'
+        print(clicks_count_msg)
+
+
 if __name__ == '__main__':
-    main()
+    get_clicks_count()
